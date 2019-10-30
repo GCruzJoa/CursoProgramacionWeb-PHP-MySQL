2035fe27690386e480114653154e37cecbb303a7
Gabriel Cruz <50724233+GCruzJoa@users.noreply.github.com>
2019-10-30 18:43:34 -0400

    Ejercicio3.php
    3.	Hacer ejemplo de combinación de identificadores con literales.

---
+++ Ejercicio3.php
@@ -0,0 +1,20 @@
+<h2>Combinacion de identificadores y literales</h2>
+<html>
+<body>
+<?php
+    //Variables
+    $string = "Algo nuevo";
+    $numero = 39;
+    echo "<p>Siempre es bueno aprender: " . $string . "</p>";
+
+    //Literales
+    echo "<p>Lebron James usa el numero: " . "(23)" . ", mientras juega con los Angeles Lakers. </p>";
+    echo "<p>Cadena de texto: " . "estoy estudiando Ingenieria de Sistemas</p>";
+    echo "<p>Clasico primer programa: " . " Hello World</p>";
+    
+    //Expresion
+    $producto = 3 * $numero;
+    echo $producto;
+?>
+</body>
+</html>
