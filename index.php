32434d12865bef47e149cc6aa5c5eccad69cfda7
Gabriel Cruz <50724233+GCruzJoa@users.noreply.github.com>
2019-10-30 16:35:46 -0400

    Index.php
    1.	Crear un archivo de extension “.php”, que servirá como global, al cual por medio de “include o require” de forma organizada añadirán todas las prácticas anteriormente trabajadas en el módulo 2

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//Programa que haga operaciones basicas con dos numeros dados, x = 144, y = 999;
+include("a.Ejercicio1.php");
+
+//Programa que muestre en pantalla variable con el nombre completo;
+include("a.Ejercicio2.php");
+
+//Realizar un conversor de monedas a traves del uso de funciones;
+include("a.Ejercicio3.php");
+
+//Programa que imprima los datos contenidos en un array asociativo;
+include("a.Ejercicio4.php");
+
+//Redireccion a otra pagina web;
+//include("b.Ejercicio1.php");
+
+//Script que muestre url actual;
+include("b.Ejercicio2.php");
+
+//Script que muestre ip del cliente, ip del servidor y nombre del fichero en ejecucion;
+include("b.Ejercicio3.php");
+
+//
+include("b.Ejercicio4.php");
+
+?>
