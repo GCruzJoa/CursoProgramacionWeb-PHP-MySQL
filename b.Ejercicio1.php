1185f5588a28ba33f3a4c0d3837b6bd1fdbda93b
Gabriel Cruz <50724233+GCruzJoa@users.noreply.github.com>
2019-12-13 12:05:11 -0400

    Anexos del archivo index.php
    El archivo index.php hace uso de la función require para llamar a estos archivos realizados en la asignación del modulo 2.

---
+++ b.Ejercicio1.php
@@ -0,0 +1,12 @@
+<html>
+<head><h1>Funciones predefinidas: Ejercicio 1</h1>
+<title>Funciones predefinidas: Ejercicio 1</title>
+
+<body>
+    <?php
+    //i.	Un script para redirigir al cliente hacia una página diferente.
+    $nuevaURL = 'https://www.Youtube.com';
+    header('Location: '. $nuevaURL);
+    ?>
+</body>
+</html>
---
+++ b.Ejercicio4.php
@@ -0,0 +1,13 @@
+<html>
+<head><h1>Funciones predefinidas: Ejercicio 4</h1>
+<title>Funciones predefinidas: Ejercicio 4</title>
+</head>
+
+<body>
+    <?php
+    print "<p>iv.	Un script que muestre la versión de PHP sin utilizar Phpinfo().</p>\n";
+    print "<p>Mi versión actual de PHP es: " . phpversion() . "</p>";
+    ?>
+
+</body>
+</html>
